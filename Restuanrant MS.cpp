3f3b4411be2e5a16a1cf376961d9e3a20493cfd1
bsmt18019@itu.edu.pk <bsmt18019@itu.edu.pk>
2022-02-10 00:35:04 +0500

    first draft

--- Restuanrant MS.cpp
+++ Restuanrant MS.cpp
@@ -3,60 +3,43 @@
 #include "Orders.h"
 using namespace std;
 
+void admin();
+void customer();
 int main()
 {
-	Items i;
-	Orders o;
-	
-	int option,id1, id2;
-	//customer control area
-	do
+	cout << "\t\t\t##########################################" <<endl;
+	cout << "\t\t\t            R E S T U A R A N T "<<endl;
+	cout<<"\t\t\t     M A N A G E M E N T   S Y S T E M" << endl;
+	cout << "\t\t\t##########################################\n" << endl<<endl;
+	int press;
+	cout << "Select 1: Customer" << endl;
+	cout << "Select 2: Admin" << endl;
+	cin >> press;
+
+	switch (press)
 	{
-		cout << "Enter your Choice" << endl;
-		cout << "1. Place a order" << endl;
-		cout << "2. Update a order" << endl;
-		cout << "3. Delete a delete" << endl;
-		cout << "4. Show Order List" << endl;
-		//cout << "5. " << endl;
-		cout << "0. Exit" << endl;
+	case 1:
+		customer();
+		break;
+	case 2:
+		admin();
+		break;
+	case 0:
+		break;
+	default:
+		cout << "Invalid entry, please try again";
+		break;
+	}
+	
+	
 
-		cin >> option;
-		switch (option)
-		{
-		case 1:
-
-			//i.add_item();
-			o.place_order();
-			break;
-		case 2:
-			cout << "update order" << endl;
-			
-			//i.edit_item(id1);
-			break;
-		case 3:
-			cout << "Order update delete" << endl;
-			
-			//o.delete_order;
-			break;
-		case 4:
-			//o.update_order();
-			break;
-		case 5:
-			//
-			break;
-		case 0:
-			break;
-		default:
-			cout << "\nInvalid Entry. Try Again";
-			break;
-		}
-
-
-
-	} while (option != 0);
-
-	//admin control area
-
+	
+}
+//admin control area
+void admin()
+{
+	Items i;
+	int option,id1, id2;
 	do
 	{
 		cout << "Enter your Choice" << endl;
@@ -84,7 +67,7 @@ int main()
 			i.delete_item(id2);
 			break;
 		case 4:
-			 i.search_item();
+			i.search_item();
 			break;
 		case 5:
 			i.show_all_item();
@@ -97,6 +80,58 @@ int main()
 
 
 	} while (option != 0);
+
+
+}
+//customer control area
+void customer()
+{
+	int option, id1, id2;
 	
+	Orders o;
+	do
+	{
+		cout << "Enter your Choice" << endl;
+		cout << "1. Place a order" << endl;
+		cout << "2. Update a order" << endl;
+		cout << "3. Delete a delete" << endl;
+		cout << "4. Show Order List" << endl;
+		//cout << "5. " << endl;
+		cout << "0. Exit" << endl;
+
+		cin >> option;
+		switch (option)
+		{
+		case 1:
+
+			//i.add_item();
+			o.place_order();
+			break;
+		case 2:
+			cout << "update order" << endl;
+
+			//i.edit_item(id1);
+			break;
+		case 3:
+			cout << "Order update delete" << endl;
+
+			//o.delete_order;
+			break;
+		case 4:
+			//o.update_order();
+			break;
+		case 5:
+			//
+			break;
+		case 0:
+			break;
+		default:
+			cout << "\nInvalid Entry. Try Again";
+			break;
+		}
+
+
+
+	} while (option != 0);
 
 }
